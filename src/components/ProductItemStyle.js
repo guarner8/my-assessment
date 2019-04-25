527f60ffbc77d0ed03aad50d8f2cb4c4e0efa503
Andres Guarneros <ag5665@nyu.edu>
2019-04-24 20:00:01 -0400

    Styled logo bar and added responsive images

---
+++ src/components/ProductItemStyle.js
@@ -0,0 +1,16 @@
+const imgStyle = {
+    zIndex: "1"
+}
+
+const productInfo = {
+    backgroundColor: "blue",
+    zIndex: "-1",
+    paddingLeft: "100px"
+}
+
+const styles = {
+    productInfo: productInfo,
+    imgStyle: imgStyle
+}
+
+export { styles }
---
+++ src/containers/AppStyles.js
@@ -0,0 +1,18 @@
+const companyLogo = {
+    fontSize: "3em",
+    fontFamily: "Chivo",
+    fontWeight: "Bold"
+}
+
+const test = {
+    backgroundColor: "",
+    marginTop: "20px"
+}
+
+const styles = {
+    companyLogo: companyLogo,
+    test: test
+    
+}
+
+export {styles}
